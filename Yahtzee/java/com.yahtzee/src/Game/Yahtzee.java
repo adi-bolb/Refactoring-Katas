76148fffb881d5b7c7c4e221848c5eefc3bd450f
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-05-21 19:37:22 +0300

    extracted ones, two and threes to local variables

--- Yahtzee/java/com.yahtzee/src/Game/Yahtzee.java
+++ Yahtzee/java/com.yahtzee/src/Game/Yahtzee.java
@@ -26,34 +26,36 @@ public class Yahtzee {
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
         int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
+        int one = 1;
+        if (d1 == one) sum +=1;
+        if (d2 == one) sum +=1;
+        if (d3 == one) sum +=1;
+        if (d4 == one) sum +=1;
+        if (d5 == one) sum +=1;
 
         return sum;
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
         int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
+        int two = 2;
+        if (d1 == two) sum += 2;
+        if (d2 == two) sum += 2;
+        if (d3 == two) sum += 2;
+        if (d4 == two) sum += 2;
+        if (d5 == two) sum += 2;
         return sum;
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
         int s;    
         s = 0;
-        if (d1 == 3) s += 3;
-        if (d2 == 3) s += 3;
-        if (d3 == 3) s += 3;
-        if (d4 == 3) s += 3;
-        if (d5 == 3) s += 3;
+        int three = 3;
+        if (d1 == three) s += 3;
+        if (d2 == three) s += 3;
+        if (d3 == three) s += 3;
+        if (d4 == three) s += 3;
+        if (d5 == three) s += 3;
         return s;
     }
 
