25ec2669f91186ef62ed66085a2281a34d21cd89
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-11-27 20:46:05 +0200

    Added a test to show that how the player name is written on win situation is wrong

--- Tennis/java/tennis/src/tennis/TennisGame1.java
+++ Tennis/java/tennis/src/tennis/TennisGame1.java
@@ -53,9 +53,12 @@ public class TennisGame1 implements TennisGame {
         {
             int minusResult = m_score1-m_score2;
             if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            else {
+                String player2 = "player2";
+                if (minusResult ==-1) score ="Advantage " + player2;
+                else if (minusResult>=2) score = "Win for player1";
+                else score ="Win for " + player2;
+            }
         }
         else
         {
