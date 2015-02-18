e09bfb1db0e10a6b86c3c8d8f6b42cff66fdada1
Adi Bolboaca <adi_bolb123@yahoo.com>
2015-02-18 20:04:36 +0200

    Removed usesless fields in class Dice

--- Yahtzee/java/com.yahtzee/src/Game/Dice.java
+++ Yahtzee/java/com.yahtzee/src/Game/Dice.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dice {
-    private final int d1;
-    private final int d2;
-    private final int d3;
-    private final int d4;
-    private final int d5;
     private List<Integer> diceValues = new ArrayList<Integer>();
 
     public Dice(int d1, int d2, int d3, int d4, int d5) {
@@ -17,12 +12,6 @@ public class Dice {
         diceValues.add(d3);
         diceValues.add(d4);
         diceValues.add(d5);
-
-        this.d1 = d1;
-        this.d2 = d2;
-        this.d3 = d3;
-        this.d4 = d4;
-        this.d5 = d5;
     }
 
     public Integer getDice(int oneBasedIndex){
