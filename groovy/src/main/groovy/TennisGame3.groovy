5ba084ab75f90ac2c30922b7331fc89eae291fd4
Adi Bolboaca <adi_bolb123@yahoo.com>
2016-06-15 15:32:41 +0300

    Extracted function equalPointsForPlayers

--- groovy/src/main/groovy/TennisGame3.groovy
+++ groovy/src/main/groovy/TennisGame3.groovy
@@ -16,16 +16,21 @@ public class TennisGame3 implements TennisGame {
         if (gameNotFinished && scoreSmallerThanForty) {
 			String[] textOfScores = ["Love", "Fifteen", "Thirty", "Forty"]
 			scoreAsText = textOfScores[playerOnePoints]
-			return (playerOnePoints == playerTwoPoints) ? scoreAsText + "-All" : scoreAsText + "-" + textOfScores[playerTwoPoints]
+
+            return equalPointsForPlayers() ? scoreAsText + "-All" : scoreAsText + "-" + textOfScores[playerTwoPoints]
 		} else {
-			if (playerOnePoints == playerTwoPoints)
+            if (equalPointsForPlayers())
 				return "Deuce"
 			scoreAsText = playerOnePoints > playerTwoPoints ? p1N : p2N
 			return ((playerOnePoints - playerTwoPoints) * (playerOnePoints - playerTwoPoints) == 1) ? "Advantage " + scoreAsText : "Win for " + scoreAsText
 		}
 	}
 
-	void wonPoint(playerName) {
+    private boolean equalPointsForPlayers() {
+        playerOnePoints == playerTwoPoints
+    }
+
+    void wonPoint(playerName) {
 		if (playerName == "player1")
 			this.playerOnePoints += 1
 		else
