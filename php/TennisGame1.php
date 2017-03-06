a5d160445c59fe0936eb2a8d412f7a755dc5e70d
Adi Bolboaca <adi_bolb123@yahoo.com>
2017-03-06 12:24:31 +0200

    Renamed variable score with gameScore in function getGameScore

--- php/TennisGame1.php
+++ php/TennisGame1.php
@@ -23,61 +23,61 @@ class TennisGame1 implements TennisGame
     }
 
     public function getGameScore(){
-        $score = "";
+        $gameScore = "";
         $tempScore=0;
         if ($this->playerOnePoints==$this->playerTwoPoints)
         {
             switch ($this->playerOnePoints)
             {
                 case 0:
-                    $score = "Love-All";
+                    $gameScore = "Love-All";
                     break;
                 case 1:
-                    $score = "Fifteen-All";
+                    $gameScore = "Fifteen-All";
                     break;
                 case 2:
-                    $score = "Thirty-All";
+                    $gameScore = "Thirty-All";
                     break;
                 case 3:
-                    $score = "Forty-All";
+                    $gameScore = "Forty-All";
                     break;
                 default:
-                    $score = "Deuce";
+                    $gameScore = "Deuce";
                     break;
             }
         }
         else if ($this->playerOnePoints>=4 || $this->playerTwoPoints>=4)
         {
             $minusResult = $this->playerOnePoints - $this->playerTwoPoints;
-            if ($minusResult==1) $score = "Advantage player1";
-            else if ($minusResult ==-1) $score = "Advantage player2";
-            else if ($minusResult>=2) $score = "Win for player1";
-            else $score = "Win for player2";
+            if ($minusResult==1) $gameScore = "Advantage player1";
+            else if ($minusResult ==-1) $gameScore = "Advantage player2";
+            else if ($minusResult>=2) $gameScore = "Win for player1";
+            else $gameScore = "Win for player2";
         }
         else
         {
             for ($i=1; $i<3; $i++)
             {
                 if ($i==1) $tempScore = $this->playerOnePoints;
-                else { $score.="-"; $tempScore = $this->playerTwoPoints;}
+                else { $gameScore.="-"; $tempScore = $this->playerTwoPoints;}
                 switch($tempScore)
                 {
                     case 0:
-                        $score.="Love";
+                        $gameScore.="Love";
                         break;
                     case 1:
-                        $score.="Fifteen";
+                        $gameScore.="Fifteen";
                         break;
                     case 2:
-                        $score.="Thirty";
+                        $gameScore.="Thirty";
                         break;
                     case 3:
-                        $score.="Forty";
+                        $gameScore.="Forty";
                         break;
                 }
             }
         }
-        return $score;
+        return $gameScore;
 
     }
 
