070159bcff1f4610c62a4b4650e663d15e71cd4c
Adi Bolboaca <adi_bolb123@yahoo.com>
2017-03-07 15:40:57 +0200

    Extracted Interface TennisGameScore

---
+++ php/TennisGameScore.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adi
+ * Date: 3/7/17
+ * Time: 3:38 PM
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: adi
+ * Date: 3/7/17
+ * Time: 12:50 PM
+ */
+interface TennisGameScore
+{
+    public function computeScore($playerOnePoints, $playerTwoPoints);
+}
