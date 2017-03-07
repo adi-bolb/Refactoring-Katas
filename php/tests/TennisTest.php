357dde9ddc1a1a2fef7bde8973839d7db08fb67b
Adi Bolboaca <adi_bolb123@yahoo.com>
2017-03-07 17:00:15 +0200

    Minimized duplication between data for single and double score implementations in the tests

--- php/tests/TennisTest.php
+++ php/tests/TennisTest.php
@@ -14,81 +14,12 @@ class TennisTest extends PHPUnit_Framework_TestCase
 	{
         $singleGameScore = $this->getSingleGameScore();
         $doubleGameScore = $this->getDoubleGameScore();
-		return(array(
-			array( 0, 0, "Love-All", $singleGameScore),
-			array( 1, 1, "Fifteen-All", $singleGameScore),
-			array( 2, 2, "Thirty-All", $singleGameScore),
-			array( 3, 3, "Forty-All", $singleGameScore),
-			array( 4, 4, "Deuce", $singleGameScore),
-			array( 1, 0, "Fifteen-Love", $singleGameScore),
-			array( 0, 1, "Love-Fifteen", $singleGameScore),
-			array( 2, 0, "Thirty-Love", $singleGameScore),
-			array( 0, 2, "Love-Thirty", $singleGameScore),
-			array( 3, 0, "Forty-Love", $singleGameScore),
-			array( 0, 3, "Love-Forty", $singleGameScore),
-			array( 4, 0, "Win for player1", $singleGameScore),
-			array( 0, 4, "Win for player2", $singleGameScore),
-
-			array( 2, 1, "Thirty-Fifteen", $singleGameScore),
-			array( 1, 2, "Fifteen-Thirty", $singleGameScore),
-			array( 3, 1, "Forty-Fifteen", $singleGameScore),
-			array( 1, 3, "Fifteen-Forty", $singleGameScore),
-			array( 4, 1, "Win for player1", $singleGameScore),
-			array( 1, 4, "Win for player2", $singleGameScore),
-
-			array( 3, 2, "Forty-Thirty", $singleGameScore),
-			array( 2, 3, "Thirty-Forty", $singleGameScore),
-			array( 4, 2, "Win for player1", $singleGameScore),
-			array( 2, 4, "Win for player2", $singleGameScore),
-
-			array( 4, 3, "Advantage player1", $singleGameScore),
-			array( 3, 4, "Advantage player2", $singleGameScore),
-			array( 5, 4, "Advantage player1", $singleGameScore),
-			array( 4, 5, "Advantage player2", $singleGameScore),
-			array( 15, 14, "Advantage player1", $singleGameScore),
-			array( 14, 15, "Advantage player2", $singleGameScore),
-
-			array( 6, 4, "Win for player1", $singleGameScore),
-			array( 4, 6, "Win for player2", $singleGameScore),
-			array( 16, 14, "Win for player1", $singleGameScore),
-			array( 14, 16, "Win for player2", $singleGameScore),
-
-
-
-            array( 0, 0, "Love-All", $doubleGameScore),
-			array( 1, 1, "Fifteen-All", $doubleGameScore),
-			array( 2, 2, "Thirty-All", $doubleGameScore),
-			array( 3, 3, "Forty-All", $doubleGameScore),
-			array( 4, 4, "Deuce", $doubleGameScore),
-			array( 1, 0, "Fifteen-Love", $doubleGameScore),
-			array( 0, 1, "Love-Fifteen", $doubleGameScore),
-			array( 2, 0, "Thirty-Love", $doubleGameScore),
-			array( 0, 2, "Love-Thirty", $doubleGameScore),
-			array( 3, 0, "Forty-Love", $doubleGameScore),
-			array( 0, 3, "Love-Forty", $doubleGameScore),
-			array( 4, 0, "Win for player1", $doubleGameScore),
-			array( 0, 4, "Win for player2", $doubleGameScore),
-
-			array( 2, 1, "Thirty-Fifteen", $doubleGameScore),
-			array( 1, 2, "Fifteen-Thirty", $doubleGameScore),
-			array( 3, 1, "Forty-Fifteen", $doubleGameScore),
-			array( 1, 3, "Fifteen-Forty", $doubleGameScore),
-			array( 4, 1, "Win for player1", $doubleGameScore),
-			array( 1, 4, "Win for player2", $doubleGameScore),
-
-			array( 3, 2, "Forty-Thirty", $doubleGameScore),
-			array( 2, 3, "Thirty-Forty", $doubleGameScore),
-			array( 4, 2, "Win for player1", $doubleGameScore),
-			array( 2, 4, "Win for player2", $doubleGameScore),
-
-			array( 4, 3, "Win for player1", $doubleGameScore),
-			array( 3, 4, "Win for player2", $doubleGameScore),
-		));
+		return(array_merge($this->getAllScoresForSingle($singleGameScore),
+            $this->getAllScoresForDouble($doubleGameScore)));
 	}
 
-    public function getAllScores()
+    public function getAllScoresForSingle($singleGameScore)
     {
-        $singleGameScore = $this->getSingleGameScore();
         return(array(
             array( 0, 0, "Love-All", $singleGameScore),
             array( 1, 1, "Fifteen-All", $singleGameScore),
@@ -166,7 +97,7 @@ class TennisTest extends PHPUnit_Framework_TestCase
 
 	/**
 	 * @test
-	 * @dataProvider getAllScores
+	 * @dataProvider getAllScoresForSingle
 	 */
 	public function checkAllScoresTennisGame2($player1Score, $player2Score, $expectedScore)
 	{
@@ -177,7 +108,7 @@ class TennisTest extends PHPUnit_Framework_TestCase
 
 	/**
 	 * @test
-	 * @dataProvider getAllScores
+	 * @dataProvider getAllScoresForSingle
 	 */
 	public function checkAllScoresTennisGame3($player1Score, $player2Score, $expectedScore)
 	{
@@ -210,6 +141,44 @@ class TennisTest extends PHPUnit_Framework_TestCase
     private function getDoubleGameScore()
     {
         return new DoubleTennisGameScore();
+    }
+
+    /**
+     * @param $doubleGameScore
+     * @return array
+     */
+    private function getAllScoresForDouble($doubleGameScore)
+    {
+        return array(
+            array(0, 0, "Love-All", $doubleGameScore),
+            array(1, 1, "Fifteen-All", $doubleGameScore),
+            array(2, 2, "Thirty-All", $doubleGameScore),
+            array(3, 3, "Forty-All", $doubleGameScore),
+            array(4, 4, "Deuce", $doubleGameScore),
+            array(1, 0, "Fifteen-Love", $doubleGameScore),
+            array(0, 1, "Love-Fifteen", $doubleGameScore),
+            array(2, 0, "Thirty-Love", $doubleGameScore),
+            array(0, 2, "Love-Thirty", $doubleGameScore),
+            array(3, 0, "Forty-Love", $doubleGameScore),
+            array(0, 3, "Love-Forty", $doubleGameScore),
+            array(4, 0, "Win for player1", $doubleGameScore),
+            array(0, 4, "Win for player2", $doubleGameScore),
+
+            array(2, 1, "Thirty-Fifteen", $doubleGameScore),
+            array(1, 2, "Fifteen-Thirty", $doubleGameScore),
+            array(3, 1, "Forty-Fifteen", $doubleGameScore),
+            array(1, 3, "Fifteen-Forty", $doubleGameScore),
+            array(4, 1, "Win for player1", $doubleGameScore),
+            array(1, 4, "Win for player2", $doubleGameScore),
+
+            array(3, 2, "Forty-Thirty", $doubleGameScore),
+            array(2, 3, "Thirty-Forty", $doubleGameScore),
+            array(4, 2, "Win for player1", $doubleGameScore),
+            array(2, 4, "Win for player2", $doubleGameScore),
+
+            array(4, 3, "Win for player1", $doubleGameScore),
+            array(3, 4, "Win for player2", $doubleGameScore),
+        );
     }
 
 }
